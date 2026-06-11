b657536d2a24d5629cf81401c1ee05c896c60a1a
Johnny Quach <Johnqdx@gmail.com>
2026-06-11 15:59:20 -0700

    Update auth.py, get_current_users not complete

--- app/auth.py
+++ app/auth.py
@@ -15,5 +15,34 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 pwd_content = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
 
+
 def hash_password(password: str) -> str:
     return pwd_content.hash(password)
+
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    return pwd_content.verify(password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(datetime.timezone.utc) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
