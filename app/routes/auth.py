f56be36a6910817a45e7ce71820777344d40cae9
Johnny Quach <84772584+JqJohnny@users.noreply.github.com>
2026-07-01 21:41:15 -0700

    Add null check for user password in login, for google sign-ins.

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -27,7 +27,7 @@ def login(
     form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
 ):
     user = db.query(User).filter(User.email == form_data.username).first()
-    if not user or not verify_password(form_data.password, user.password):
+    if not user or not user.password or not verify_password(form_data.password, user.password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect email or password",
