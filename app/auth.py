e2574d3bd2146469c4b577231470f373398dcc94
Johnny Quach <84772584+JqJohnny@users.noreply.github.com>
2026-07-12 22:23:44 -0700

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -4,6 +4,7 @@ from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
+from authlib.integrations.starlette_client import OAuth
 from .database import get_db
 from .models import User
 import os
